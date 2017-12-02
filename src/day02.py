03fef1607d0bf032b49a5a85df00ce87b99d1297
Marc <barcmoehm@gmail.com>
2017-12-02 16:07:36 +0100

    day 2 - challenge 2 completed

--- src/day02.py
+++ src/day02.py
@@ -3,13 +3,29 @@ def checkSum1(fname):
         content = file.readlines()
 
     sum = 0
-    content = [line.strip().split() for line in content]
+    content = [[int(d) for d in line.strip().split()] for line in content]
     for line in content:
-        
-        line= list(map(int, line))
         sum += (max(line) - min(line))
     print(sum)
     return sum
 
+def checkSum2(fname):
+    with open(fname) as file:
+        content = file.readlines()
+    sum = 0
+    content = [[int(d) for d in line.strip().split()] for line in content]
+    for line in content:
+        size = len(line)
+        flag = False
+        for i in range(0, size):
+            for j in range(0, size):
+                part1 = max(line[i], line[j])
+                part2 = min(line[i], line[j])
+                if(i != j and not flag and (part1%part2 == 0)):
+                    sum+= part1//part2
+                    flag = True
+    return sum
 
 checkSum1('day02.txt')
+checkSum2('day02.txt')
+
