48814549de8b35318965f13ed389d1bf8707b8a9
Marc <barcmoehm@gmail.com>
2017-12-03 17:31:59 +0100

    day 3 - challenge 2 completed
    had to consult external resources for the solution

--- src/day03.py
+++ src/day03.py
@@ -1,9 +1,14 @@
-sums = 1
-add = 1
-n = 368078
-counter = 0
-while(sums<=n):
-    counter+=1
-    add += 8
-    sums += add
-    print(counter + (n- sums))
+import urllib.request
+def solve1():
+    sums = 1
+    add = 1
+    n = 368078
+    counter = 0
+    while(sums<=n):
+        counter+=1
+        add += 8
+        sums += add
+        print(counter + (n- sums))
+
+## https://oeis.org/A141481/b141481.txt")
+solve1()
