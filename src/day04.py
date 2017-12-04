6e09b84f0ee4e62d63976551556e16c9362efd8a
Marc Böhm <marc.boehm@get-ag.com>
2017-12-04 15:49:09 +0100

    day 04 - challenge 2 completed
    I felt a lot more comfortable writing python today. advent of code really is a good projecto start learning a new language!

--- src/day04.py
+++ src/day04.py
@@ -4,4 +4,17 @@ def solve1(filename):
         return [sum([li.count(s) for s in li]) == len(li) for li in content].count(True)
 
 
+def solve2(filename):
+    count = 0
+    with open(filename) as file:
+        content = [line.strip().split() for line in file.readlines()]
+        for line in content:
+            sort = ["".join(sorted(s)) for s in line]
+            if len(sort) == len(set(sort)):
+                count += 1
+
+    return count
+
+
 print(solve1("day04.txt"))
+print(solve2("day04.txt"))
