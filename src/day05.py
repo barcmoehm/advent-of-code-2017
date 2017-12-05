be74a88300f239a18d033323f92e0c280011196f
Marc Böhm <marc.boehm@get-ag.com>
2017-12-05 13:21:46 +0100

    day 05 - challenge 2 completed

--- src/day05.py
+++ src/day05.py
@@ -12,5 +12,19 @@ def solve1(filename):
     return steps
 
 
+def solve2(filename):
+    with open(filename) as file:
+        content = [int(line.strip()) for line in file]
+
+    idx = 0
+    steps = 0
+    while 0 <= idx < len(content):
+        i = idx
+        idx += content[idx]
+        content[i] += 1 if content[i] < 3 else -1
+        steps += 1
+    return steps
+
 
 print(solve1("day05.txt"))
+print(solve2("day05.txt"))
