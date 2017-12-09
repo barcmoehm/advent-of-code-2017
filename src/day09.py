07d96bc15d2ec0a393d71ff0886930a057427e36
Marc Böhm <barcmoehm@gmail.com>
2017-12-09 18:52:33 +0100

    day 09 - challenge 2 completed

--- src/day09.py
+++ src/day09.py
@@ -1,14 +1,24 @@
 import re
+from functools import reduce
+
+
+def match_len(x):
+    return len(x) - 2
+
 
 with open("day09.txt") as file:
     content = [line.strip() for line in file]
     pattern = re.compile(r"<.*?>")
     pattern2 = re.compile("!.")
+
+    def add_lens(x, y) : return x + y
+
     for line in content:
+        line = pattern2.sub("", line)
+        garbage_count2 = reduce(add_lens, map(match_len, pattern.findall(line)))
         idx = 0
         count = 0
         depth = 0
-        line = pattern2.sub("", line)
         line = pattern.sub("", line)
         while idx < len(line):
             if line[idx] == "{":
@@ -18,6 +28,8 @@ with open("day09.txt") as file:
                 depth -= 1
             idx += 1
         print(count)
+        print(garbage_count2)
+
 
 
 
