7b7acec4bb3be2621fc0d95f172d9faa7634d6b8
Marc Böhm <barcmoehm@gmail.com>
2017-12-17 17:57:47 +0100

    day 17 - challenge 2 completed

--- src/day17.py
+++ src/day17.py
@@ -1,8 +1,11 @@
-lst = [0]
+n = 0
 steps = 335
 pos = 0
-for i in range(2017):
-    pos = (pos + steps) % len(lst) + 1
-    lst.insert(pos, i + 1)
-print(lst, pos)
-print(lst[pos+1])
+for i in range(1, 50000001):
+    pos = (pos + steps) % i + 1
+    if pos == 1:
+        n = i
+print(n)
+
+
+
