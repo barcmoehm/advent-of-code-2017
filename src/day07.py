58613460e89a4403796a139906a72c98b2538c01
Marc Böhm <marc.boehm@get-ag.com>
2017-12-22 10:13:30 +0100

    day 07 - challenge 2 completed#

--- src/day07.py
+++ src/day07.py
@@ -1,16 +1,37 @@
+from collections import defaultdict
+
 content = [line.strip().split() for line in open("day7.txt").readlines()]
 programs = []
 tops = set()
+weights = {}
+children = defaultdict(list)
+
 for line in content:
     programs.append(line[0])
+    weights[line[0]] = int(line[1].strip('()'))
+
     if '->' in line:
         i = line.index('->')
-        children = [c.strip(',') for c in line[i + 1:]]
-        for c in children:
-            tops.add(c)
+        temp = children[programs[-1]] = [c.strip(',') for c in line[i + 1:]]
+        for t in temp:
+            tops.add(t)
 
 for p in programs:
     if p not in tops:
-        print(p)
+        root = p
 
 
+def unbalanced(children, weights, root):
+    expected = None
+    wsum = weights[root]
+    for c in children[root]:
+        c_weight = unbalanced(children, weights, c)
+        wsum += c_weight
+        if expected is None:
+            expected = c_weight
+        elif expected != c_weight:
+            print(expected - (c_weight - weights[c]))
+    return wsum
+
+
+unbalanced(children, weights, root)
